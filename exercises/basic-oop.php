e098e43941feaee733e1f307794d09367927ec89
Maiko Matsuoka <117214172+NIMUchanco@users.noreply.github.com>
2023-10-26 13:51:40 -0400

    Added basic-oop.php

---
+++ exercises/basic-oop.php
@@ -0,0 +1,44 @@
+<?php
+    class Backpack {
+        public $name;
+        public $color;
+        public $size;
+        public $pocketNum;
+        public $strapLen;
+        public $zipper = " is closed.";
+
+        public function __construct($name, $color, $size, $pocketNum, $strapLen){
+            $this->name = $name;
+            $this->color = $color;
+            $this->size = $size;
+            $this->pocketNum = $pocketNum;
+            $this->strapLen = $strapLen;
+        }
+
+        public function zippStatus($zipper){
+            if ($zipper) {
+                $this->zipper = " is open.";
+            } 
+        }
+
+        public function carry($item){
+            echo $this->name . " is carrying " . $item . ".";
+        }
+    }
+
+    $backpack1 = new Backpack("daypack", "orange", "20L", "5", "20cm");
+    $backpack2 = new Backpack("travelpack", "black", "40L", "10", "40cm");
+
+    echo "The 1st backpack is called " . $backpack1->name . " and it is " . $backpack1->color . ".<br><br>";
+    echo $backpack1->carry("a laptop") . "<br><br>";
+    $backpack1->zippStatus(true);
+    echo $backpack1->name . $backpack1->zipper . "<br><br>";
+    echo "Let's start packing! <br><br>" ;
+
+    echo "======================================<br><br>";
+    echo "The 2nd backpack is called " . $backpack2->name . " and it is " . $backpack2->size . ".<br><br>";
+    echo $backpack2->carry("a lunch box") . "<br><br>";
+    $backpack2->zippStatus(false);
+    echo $backpack2->name . $backpack2->zipper . "<br><br>";
+
+?>
--- exercises/class1.php
+++ exercises/class1.php
@@ -56,7 +56,7 @@
     echo "<br><br>";
 ?>
 <?php
-
+    echo "[Homework]<br>";
     // ini_set('display_errors', 1);
     // ini_set('display_startup_errors', 1);
     // error_reporting(E_ALL);
@@ -75,20 +75,20 @@
         // echo "<br>";
 
         //origial word without spaces
-        for ($j = 0 ; $j < $count; $j++) {
-            if ($word[$j] == " ") {
+        for ($i = 0 ; $i < $count; $i++) {
+            if ($word[$i] == " " || $word[$i] == ".") {
                 continue;
-            } else if ($word[$j] >= 'A' && $word[$j] <= 'Z') {
-                $words .= chr(ord($word[$j]) + 32);
+            } else if ($word[$i] >= 'A' && $word[$i] <= 'Z') {
+                $words .= chr(ord($word[$i]) + 32);
                 // $words .= (string) (((int) $word[$j]) + 32) ;
             } else {
-                $words .= $word[$j];
+                $words .= $word[$i];
             }
         }
 
         //reverse without spaces
-        for ($i = $count -1; $i >= 0; $i--) {
-            $reverse .= $words[$i];
+        for ($j = $count -1; $j >= 0; $j--) {
+            $reverse .= $words[$j];
         }
 
 
@@ -104,10 +104,38 @@
         }
     }
 
-    checkPalindrome("taco cat");
+    checkPalindrome("taco cat...");
     echo "<br>";
     checkPalindrome("Taco cat");
     echo "<br>";
     checkPalindrome("adam");
 
+?>
+<?php
+
+echo "<br><br>[Class3]";
+
+$phrase = ".....taco   cat..";
+$forwardi = 0;
+$backi = strlen($phrase) - 1;
+
+for ($i = 0; $i < strlen($phrase); $i++) {
+    while ($forwardi < $backi && ($phrase[$forwardi] == " " || $phrase[$forwardi] == ".")) {
+        $forwardi++;
+    }
+    while ($forwardi < $backi && ($phrase[$backi] == " " || $phrase[$backi] == ".")) {
+        $backi--;
+    }
+    if ($backi < $forwardi) {
+        echo "this is a palindrome";
+        break;
+    }
+    if ($phrase[$forwardi] != $phrase[$backi]) {
+        echo "this is not a palindrome";
+        break;
+    }
+    echo "<br>$i $phrase[$forwardi] $phrase[$backi]<br>";
+    $forwardi++;
+    $backi--;
+}
 ?>
